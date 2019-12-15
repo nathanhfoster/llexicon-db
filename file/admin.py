6d09d5535475948e871957cf3171f30f282cb824
Nathan  Foster <nateinthegame@gmail.com>
2019-12-15 11:11:06 -0800

    File Fixes

--- file/admin.py
+++ file/admin.py
@@ -12,15 +12,16 @@ class FileResource(resources.ModelResource):
 
     class Meta:
         model = File
-        fields = ('id', 'entry_id', 'media_type', 'url',
-                  'date_created', 'date_updated',)
+        fields = ('id', 'entry_id',
+                  'file_type', 'name', 'size', 'url',
+                  'date_created', 'date_updated', 'date_modified',)
 
 
 class FileAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     resource_class = FileResource
-    list_display = (
-        'id', 'entry_id', 'media_type', 'url',
-        'date_created', 'date_updated',)
+    list_display = ('id', 'entry_id',
+                    'file_type', 'name', 'size', 'url',
+                    'date_created', 'date_updated', 'date_modified',)
 
 
 admin.site.register(File, FileAdmin)
--- file/models.py
+++ file/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 from diary.models import Entry
+from django.utils.timezone import now
 
 
 class File(models.Model):
-    TYPES = (
-        ('Image', 'Image'),
-        ('Video', 'Video'),
-        ('File', 'File'),
-    )
+    # TYPES = (
+    #     ('Image', 'Image'),
+    #     ('Video', 'Video'),
+    #     ('File', 'File'),
+    # )
+    # choices=TYPES
+
     entry_id = models.ForeignKey(
         Entry,
         related_name='EntryFiles',
         on_delete=models.CASCADE, )
-    media_type = models.CharField(blank=True, max_length=6, choices=TYPES)
+    file_type = models.CharField(blank=True, max_length=24)
+    name = models.CharField(blank=True, max_length=512)
+    size = models.IntegerField(default=0)
     url = models.FileField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    date_modified = models.DateTimeField(default=now())
 
     class Meta:
         verbose_name = 'File'
