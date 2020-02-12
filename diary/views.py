af5dc8a7952a69046922d36233611bed4b685e46
Nathan Foster <nateinthegame@gmail.com>
2020-02-12 04:04:30 -0800

    Update views.py

--- diary/views.py
+++ diary/views.py
@@ -14,7 +14,7 @@ from rest_framework.filters import SearchFilter
 
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 25
     page_size_query_param = 'page_size'
     # max_page_size = 500
 
